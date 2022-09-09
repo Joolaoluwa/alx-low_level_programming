ea94e7fcfa2a75f85a568efbeca4a1ced5122e76
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-09 09:25:39 -0700

    This program is used to print greater and less than values.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,8 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	else
-		printf("%d is zero", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 
 	return (0);
 }
