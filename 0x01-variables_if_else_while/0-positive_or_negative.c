e6bb75473e9e66d8719dd893bc743151f957340f
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-09 09:21:12 -0700

    This program is used to print greater and less than values.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
+
 /**
  * main - Entry point
  *
@@ -13,12 +14,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* Your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);	
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero", n);
+
 	return (0);
 }
