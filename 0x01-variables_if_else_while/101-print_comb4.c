07e40009d11dab4864e25d677e5b6dcdc213b2ac
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-09 18:45:14 -0700

    This program prints all possible 3 digit combinations.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '8'; a++)
+	{
+		for (b = '1'; b < '9'; b++)
+		{
+			for (c = '2'; c <= '9'; c++)
+			{
+				if (b < c && a < b)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 7 || b != 8 || c != 9)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
