02d50f9aa459fdfbcbfd13a63baca26328b38861
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-09 17:19:47 -0700

    This program prints all possible 2 digit combination.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+	
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 8 || b != 9)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
