49524dfd7236546fa94fc7346b0cc0539147219b
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-13 22:18:14 -0700

    This program is used to return the sum of two integers.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: adds two integers and returns the result
+ * @num1: integer value recieved
+ * @num2: integer value recieved
+ * Return: sum of integer 
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
