8304a75cf9b3229c8b838c15ea1a9b2e864fc5ff
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-14 07:28:56 -0700

    This program prints n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,41 +10,43 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	for (x = 0; x <= n; x++)
+	if (n >= 0 && n <= 14)
 	{
-		for (y = 0; y <= n; y++)
+		for (x = 0; x <= n; x++)
 		{
-			z = x * y;
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
 
-			if (z > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((z / 100) + '0');
-				_putchar(((z / 10) % 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(((z / 10) % 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else
-			{
-				if (y != 0)
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				_putchar(z + '0');
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(z + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-
 }
