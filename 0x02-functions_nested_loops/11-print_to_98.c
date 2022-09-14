26e35e9b370e098102255a816f16ec59aea411b8
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-14 01:47:35 -0700

    This program is used to print numbers from n to 98.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ * Description: prints all natural numbers from n to 98
+ * Return: collection of numbers
+ * @n: integer value recieved
+ */
+void print_to_98(int n)
+{
+	int m = n;
+
+	for (m; m <= 98; m++)
+	{
+		printf("%d", m);
+		
+		if (m != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	for (m; m >= 98; m--)
+	{
+		printf("%d", m);
+
+		if (m != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
