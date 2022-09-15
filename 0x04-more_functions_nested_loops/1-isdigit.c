57ee484bbed2b0304f503666618ade5edceb7eda
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-15 04:24:38 -0700

    This program is used to check for digits 0-9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - Entry point
- * Description:  checks for a digit (0 through 9)
+ * _isdigit - checks for a digit (0 through 9)
  * @c: integer value recieved
  * Return: 1 if true. 0 if otherwise
  */
@@ -11,7 +10,7 @@ int _isdigit(int c)
 {
 	int ch;
 
-	for (ch = '0'; ch <= 9; ch++)
+	for (ch = '0'; ch <= '9'; ch++)
 	{
 		if (c == ch)
 			return (1);
