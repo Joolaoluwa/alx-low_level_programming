e3444caffa9c0a84ce2839b89b8273ab1388f297
Olusola joshua <joolaoluwa2002@gmail.com>
2022-09-26 15:02:05 -0700

    This function is used to get the first occurrente of a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strchr - a function that locates a character in a string
+ * _strchr - a function that locates a character in a string
  * @s: string searched
  * @c: character to find
  * Return: a pointer to the first occurrence of the character c,
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	 int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
